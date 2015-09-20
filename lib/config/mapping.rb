cc62f44fab2629dabad4fbbad7d0fd13cf2d27e9
My Mai <my.mai@asnet.com.vn>
2015-09-20 23:14:42 +0700

    Mapping data

--- lib/config/mapping.rb
+++ lib/config/mapping.rb
@@ -1,7 +1,13 @@
-# collection :users do
-#   entity     User
-#   repository UserRepository
-#
-#   attribute :id,   Integer
-#   attribute :name, String
-# end
+collection :users do
+  entity User
+  repository UserRepository
+
+  attribute :id, Integer
+  attribute :name, String
+  attribute :phone_number, String
+  attribute :email, String
+  attribute :pin, String
+  attribute :auth_token, String
+  attribute :created_at, DateTime
+  attribute :updated_at, DateTime
+end
